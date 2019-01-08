c334b8bd7202ad5dca139fd18389b363bd13ab39
xuchangjian <xuchangjian@hefantv.com>
2019-01-08 16:48:47 +0800

    完成头部样式

--- src/contants/index.js
+++ src/contants/index.js
@@ -1,3 +1,4 @@
 export default {
-    BASE_IMG_URL: 'https://dj001.oss-cn-beijing.aliyuncs.com/' // 图片公共访问前缀
+    BASE_IMG_URL: 'https://dj001.oss-cn-beijing.aliyuncs.com/', // 图片公共访问前缀
+    DEFAULT_COLOR: '#409EFF'
 }
--- src/views/layout/components/Header.vue
+++ src/views/layout/components/Header.vue
@@ -1,24 +1,94 @@
 <template>
   <div id="Header">
-    <Breadcrumb></Breadcrumb>
+    <div class="header_left">
+      <Breadcrumb/>
+    </div>
+    <div class="header_right">
+      <theme-picker class="theme-switch right-menu-item"/>
+      <div class="user_box">
+        <el-dropdown size="medium">
+          <span class="el-dropdown-link">
+            {{userName || '佚名'}}
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>
+              <div @click="loginOut_clickHandler">退出</div>
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Breadcrumb from '@components/Ui/Breadcrumb/index.vue'
+import ThemePicker from '@components/Ui/ThemePicker'
+import { mapActions, mapGetters } from 'vuex'
+import Utils from '@utils/utils'
+
 export default {
   name: 'Header',
   components: {
-    Breadcrumb
+    Breadcrumb,
+    ThemePicker
   },
-  created() { }
+  data() {
+    return {
+      showPopFlag: false, // 控制用户信息弹出框显示隐藏
+      userName: ''
+    }
+  },
+  computed: {
+    ...mapGetters({
+      // userInfo: "getUserInfo",
+      // stores: "getStores",
+      // storeLen: "getStoreLen"
+    })
+  },
+  props: [],
+  methods: {
+    ...mapActions(['loginOut']),
+    /**
+     * 登出事件
+     * */
+    loginOut_clickHandler() {
+      this.loginOut()
+    }
+  },
+  created() {
+    const { _userName } = Utils.getCookie()
+    this.userName = _userName
+  },
+  mounted() { },
+  destroyed() { },
+  watch: {}
 }
 </script>
 <style lang="scss" scoped>
 #Header {
+  padding: 5px;
   height: 50px;
   line-height: 50px;
   width: 100%;
+  .header_left {
+    float: left;
+  }
+  .header_right {
+    float: right;
+    .user_box {
+      display: inline-block;
+      margin: 0 15px;
+      .el-dropdown-link {
+        cursor: pointer;
+        color: #409eff;
+      }
+      .el-icon-arrow-down {
+        font-size: 12px;
+      }
+    }
+  }
 }
 </style>
 
